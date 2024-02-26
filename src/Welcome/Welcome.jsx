507696a309f4ca879fce25edbcdefee2d39db97b
Nagarjuna <nagarjunaome@gmail.com>
2024-02-26 23:22:37 +0530

    welcome page

--- src/Welcome/Welcome.jsx
+++ src/Welcome/Welcome.jsx
@@ -10,7 +10,15 @@ function Welcome() {
   };
   return (
       <div className='divEle'>
-              <h1 className='headEle'><span className='welEle'>WELCOME</span><br/> to FLAVOR-SAGE</h1>
+              <h1 className='headEle'>
+                    <span  className='welEle'>W</span>
+                    <span  className='welEle'>E</span>
+                    <span  className='welEle'>L</span>
+                    <span  className='welEle'>C</span>
+                    <span  className='welEle'>O</span>
+                    <span  className='welEle'>M</span>
+                    <span  className='welEle'>E</span>
+                <br/> to FLAVOR-SAGE</h1>
               <p className='paraEle'>
                 Discover a world of culinary delights tailored just for you. Whether you're a cooking enthusiast or simply looking for new flavours to 
                 savour, our smart recipe recommender is here to guide you on a personalized culinary journey.
